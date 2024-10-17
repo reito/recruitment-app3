19c645ecf54ea0f278a8f4fc8da8071e7668d361
reito <zhenlinshannei@gmail.com>
2024-10-18 01:24:02 +0900

    ssl

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -8,8 +8,7 @@ console.log("DATABASE_URL:", process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  // ssl: { rejectUnauthorized: false },
-  ssl: true,
+  ssl: { rejectUnauthorized: false },
 });
 
 export async function GET(request: Request) {
